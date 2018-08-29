0502532b364e325e5848f12ff994cf2981b47e4e
Katsutoshi Okumura <katsutoshiokumura@gmail.com>
2018-08-29 15:35:38 +0900

    1st commit

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+// Import everything from express and assign it to the express variable
+import express from 'express';
+
+// Import WelcomeController from controllers entry point
+import { WelcomeController } from './controllers';
+
+// Create a new express application instance
+const app: express.Application = express();
+// The port the express app will listen on
+const port: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+
+// Mount public directory at the / route
+app.use('/', express.static('./public'));
+
+// Mount the WelcomeController at the /welcome route
+app.use('/welcome', WelcomeController);
+
+// Serve the application at the given port
+app.listen(port, () => {
+  // Success callback
+  console.log(`Listening at http://localhost:${port}/`);
+});
